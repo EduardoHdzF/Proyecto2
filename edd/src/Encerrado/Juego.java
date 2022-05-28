4a661ceaf5898676f5e48480f6b3ab6d41c560c9
EduardoHdzF <eduardohf@ciencias.unam.mx>
2022-05-28 01:35:17 -0500

    ohoh

--- edd/src/Encerrado/Juego.java
+++ edd/src/Encerrado/Juego.java
@@ -14,6 +14,7 @@ public class Juego{
 	private Scanner entrada;
 	private Cola<Jugador> jugadores;
 	private Tablero tablero;
+	private boolean maquina; 
 	
 	/**
 	 * Crea la interfaz de usuario para manejar el juego.
@@ -22,6 +23,7 @@ public class Juego{
 		jugadores = new Cola<Jugador>();	
 		entrada = new Scanner(System.in);				
 		tablero = new Tablero();
+		maquina = false;
 	}
 	
 	/*
@@ -44,20 +46,40 @@ public class Juego{
 		System.out.println("     Ingrese el nombre del Jugador 1");
 		String n = entrada.nextLine();
 		Jugador q = new Jugador();
-		q.setNombre(n);
+		if(n != "" || n != null){
+			q.setNombre(n);
+		}else{
+			q.setNombre("Jugador 1");
+		}
+		
 		jugadores.push(q);
+		
 		System.out.println("\n     Desea tener un jugador 2 ");
 		System.out.println(" Digite s (sí), n (no) y después presione ENTER");
+		
 		String r = entrada.nextLine();
+		
 		if(r.equals("s")){
 		 	
 		 	System.out.println("\n     Ingrese el nombre del Jugador 2");
 		 	n = entrada.nextLine();
 		 	q = new Jugador();
-		 	q.setNombre(n);
+		 	
+		 	if(n != "" || n != null){
+				q.setNombre(n);
+			}else{
+				q.setNombre("Jugador 1");
+			}
+		 	
 		 	jugadores.push(q);
-		}else if(r.equals("n")) System.out.println("\n>> Jugará con la máquina <<");
-		
+		 	
+		}else if(r.equals("n") || r.equals("")){
+		 	
+		 	System.out.println("\n>> Jugará con la máquina <<\n");
+		 	
+		 	maquina = true;
+		 	
+		}
 		
 		
 	}
@@ -66,12 +88,36 @@ public class Juego{
      * Nos da la bienvenida y es en escencia donde se mueve el juego.
      */
     public void corre(){
-	
+		System.out.println("\033[H\033[2J");
+		System.out.flush();
 		Bienvenida();
 		Jugadores();
 		
 		tablero.dibujaTablero(0);		
-		System.out.println(tablero.dibujaFicha());
+		tablero.dibujaFicha(0);
+		
+		System.out.println("\n Presione ENTER para continuar");
+		entrada.nextLine();
+		System.out.println("\033[H\033[2J");
+		System.out.println("\n        Escoja la posición de inicial de las fichas \n");
+		System.out.println("      Numerando de izquieda a derecha, eligiendo la posición deseada\n");
+		System.out.println("Presione w si quiere poner una posición deseada o ENTER si quiere la posición por defecto\n");
+		
+		
+		String d = entrada.nextLine();
+		System.out.println("\nFichas de cada jugador\n");
+		tablero.dibujaFicha(1);
+		if(d.equals("w")){
+			
+			//Se debe poner que el usuario pueda elegir la posición de las fichas
+		}else{
+			System.out.println("\nPosición inicial por defecto\n");
+			tablero.pInicial();	
+		}
+		//System.out.println("\033[H\033[2J");
+		
+		//System.out.close();
+		//tablero.dibujaFicha(1);
 		
     }
     
--- edd/src/Encerrado/Tablero.java
+++ edd/src/Encerrado/Tablero.java
@@ -9,14 +9,35 @@ import edd.src.Estructuras.*;
  */
 public class Tablero{
 
-	String dibujo;
-	String ficha;
+	private String dibujo;
+	private String ficha;						
+	private boolean e1;
+	private boolean e2;
+	private boolean e3;
+	private boolean e4;
+				
+	String f1 = "⣴⣿⣿⣿";
+	String ff1 = "⠻⣿⣿⠟";
+	
+	String f2 = "⣴⣿⣿⣦";
+	String ff2 = "⠻⣿⣿⢿";
+
+	String f3 = "⣠⣾⣿⣷⡄";
+	String ff3 = "⣹⢿⣿⡿⠃";
+					
+	String f4 = "⢱⣿⣿⣿⡄";
+	String ff4 = "⠘⢿⣿⡿⠃";
+	
 	
 	
 	public Tablero(){
 		
 		dibujo = new String();
 		ficha = new String();
+		e1 = false;
+		e2 = false;
+		e3 = false;
+		e4 = false;
 	}
 	
 	/**
@@ -55,10 +76,11 @@ public class Tablero{
 			dibujo = dibujo +"\033[1m \033[107m 3 \033[49m                                                      \033[107m 4 \033[0m\033[49m";
 		}else {
 			
+			
 			dibujo = "";
-			dibujo = "\033[1m\033[107m 1 \033[49m                                       \033[107m 2 \033[0m\033[49m\n";
-			dibujo = dibujo + "⣴⣿⣿⣦⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣠⣾⣿⣷⡄\n";
-			dibujo = dibujo + "⠻⣿⣿⢿⡉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⣹⢿⣿⡿⠃\n";
+			dibujo = "\033[1m\033[107m 2 \033[49m                                       \033[107m 3 \033[0m\033[49m\n";
+			dibujo = dibujo + f2 + "⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀"+ f3 + "\n";
+			dibujo = dibujo + ff2 + "⡉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉" + ff3 + "\n";
 			dibujo = dibujo + "⠄⢰⡇⠄⠙⠢⣄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⡴⠚⠁⠄⣿⠄⠄\n";
 			dibujo = dibujo + "⠄⢸⡇⠄⠄⠄⠈⠳⢤⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣠⠔⠋⠄⠄⠄⠄⣿⠄⠄\n";
 			dibujo = dibujo + "⠄⢸⡇⠄⠄⠄⠄⠄⠄⠙⠦⣄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⡤⠚⠁⠄⠄⠄⠄⠄⠄⣿⠄⠄\n";
@@ -74,19 +96,53 @@ public class Tablero{
 			dibujo = dibujo + "⠄⢸⡇⠄⠄⠄⠄⠄⠄⣠⠴⠋⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⠓⢤⡀⠄⠄⠄⠄⠄⠄⣿⠄⠄\n";
 			dibujo = dibujo + "⠄⢸⡇⠄⠄⠄⢀⡤⠞⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠙⠦⣄⠄⠄⠄⠄⣿⠄⠄\n";
 			dibujo = dibujo + "⠄⢸⡇⠄⣀⠴⠋⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⠳⢤⡀⠄⣿⠄⠄\n";
-			dibujo = dibujo + "⣴⣿⣿⣿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢱⣿⣿⣿⡄\n";
-			dibujo = dibujo + "⠻⣿⣿⠟⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⢿⣿⡿⠃\n";
-			dibujo = dibujo + "\033[1m\033[107m 3 \033[49m                                       \033[107m 4 \033[0m\033[49m\n";
+			dibujo = dibujo + f1 + "⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄" + f4 + "\n";
+			dibujo = dibujo + ff1 + "⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄" + ff4 + "\n";
+			dibujo = dibujo + "\033[1m\033[107m 1 \033[49m                                       \033[107m 4 \033[0m\033[49m\n";
 		}
 		System.out.println(dibujo);
 	}
 	
-	public String dibujaFicha(){
+	public void dibujaFicha(int o){
 	
-		ficha = "  \033[91m⢠⣶⣿⣿⣶⡄    \033[96m⢠⣶⣿⣿⣶⡄    \033[91m⢠⣶⣿⣿⣶⡄    \033[96m⢠⣶⣿⣿⣶⡄\n";
-		ficha = ficha + "  \033[91m⣿⣿⣿⣿⣿⣿    \033[96m⣿⣿⣿⣿⣿⣿    \033[91m⣿⣿⣿⣿⣿⣿    \033[96m⣿⣿⣿⣿⣿⣿\n";
-		ficha = ficha + "  \033[91m⠘⠿⣿⣿⠿⠃    \033[96m⠘⠿⣿⣿⠿⠃    \033[91m⠘⠿⣿⣿⠿⠃    \033[96m⠘⠿⣿⣿⠿⠃\033[39m";
-		return ficha;
+		if(o == 0){
+			System.out.println("  \033[91m⢠⣶⣿⣿⣶⡄    \033[96m⢠⣶⣿⣿⣶⡄    \033[91m⢠⣶⣿⣿⣶⡄    \033[96m⢠⣶⣿⣿⣶⡄");
+			System.out.println("  \033[91m⣿⣿⣿⣿⣿⣿    \033[96m⣿⣿⣿⣿⣿⣿    \033[91m⣿⣿⣿⣿⣿⣿    \033[96m⣿⣿⣿⣿⣿⣿");;
+			System.out.println("  \033[91m⠘⠿⣿⣿⠿⠃    \033[96m⠘⠿⣿⣿⠿⠃    \033[91m⠘⠿⣿⣿⠿⠃    \033[96m⠘⠿⣿⣿⠿⠃\033[39m");
+		}else if( o == 1){
+		
+			System.out.println("            \033[91m⢠⣶⣿⣿⣶⡄    \033[91m⢠⣶⣿⣿⣶⡄   ");
+			System.out.println("\033[93mJugador 1:  \033[91m⣿⣿⣿⣿⣿⣿    \033[91m⣿⣿⣿⣿⣿⣿   ");
+			System.out.println("            \033[91m⠘⠿⣿⣿⠿⠃    \033[91m⠘⠿⣿⣿⠿⠃  \n\033[39m");		
+			
+			System.out.println("            \033[96m⢠⣶⣿⣿⣶⡄    \033[96m⢠⣶⣿⣿⣶⡄");
+			System.out.println("\033[93mJugador 2:  \033[96m⣿⣿⣿⣿⣿⣿    \033[96m⣿⣿⣿⣿⣿⣿");
+			System.out.println("            \033[96m⠘⠿⣿⣿⠿⠃    \033[96m⠘⠿⣿⣿⠿⠃\033[39m");
+		
+		}
+		
+			
+	}
+	
+	public void pInicial(){
+		
+		f2 = "\033[96m" + f2 + "\033[39m";
+		ff2 = "\033[96m" + ff2 + "\033[39m";
+		e2 = true;
+		
+		f4 = "\033[96m" + f4 + "\033[39m";
+		ff4 = "\033[96m" + ff4 + "\033[39m";
+		e4 = true;
+		
+		f1 = "\033[91m" + f1 + "\033[39m";
+		ff1 = "\033[91m" + ff1 + "\033[39m";
+		e1 = true;
+		
+		f3 = "\033[91m" + f3 + "\033[39m";
+		ff3 = "\033[91m" + ff3 + "\033[39m";
+		e3 = true;
+		
+		dibujaTablero(2);
 		
 	}
 }
